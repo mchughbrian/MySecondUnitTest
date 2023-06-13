cc3b98eb5b00d6b0824bdb1ceee316c513188085
brianmchugh <brianmchugh02@gmail.com>
2023-06-12 20:37:20 -0400

    Added setUp function in unittest

--- main_test.py
+++ main_test.py
@@ -1,7 +1,16 @@
 import unittest
+from main import BankAccount
 
 
-class MyTestCase(unittest.TestCase):
+
+
+class TestBankAccount(unittest.TestCase):
+
+    def setUp(self):
+        #This means that each test case starts with
+        # A fresh BankAccount object with a balance of 100
+        self.account = BankAccount("Test Account", 100)
+
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
 
