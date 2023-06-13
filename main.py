c4f11553b018c2f56fdc580d2d44d33a4f59822b
brianmchugh <brianmchugh02@gmail.com>
2023-06-12 20:25:18 -0400

    Check withdrawing too much funds

--- main.py
+++ main.py
@@ -45,3 +45,15 @@ account_mike = BankAccount("Mike's Account", 200)
 account_mike.deposit(50)
 print(account_mike.check_balance())  # Outputs: 250
 
+# Withdraw 20 from the account
+account_john.withdraw(20)
+print(account_john.check_balance())  # Outputs: 130
+
+# Attempt to withdraw 250 from Jane's account
+# This will raise a ValueError because there are insufficient funds
+try:
+    account_john.withdraw(250)
+except ValueError as e:
+    print(e)  # Outputs: Insufficient funds.
+
+
