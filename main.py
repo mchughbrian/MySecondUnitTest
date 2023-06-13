76f2a0a4cb3b390efb9e218bde3361e5e5a283cf
brianmchugh <brianmchugh02@gmail.com>
2023-06-12 20:51:03 -0400

    Latest of 6/12

--- main.py
+++ main.py
@@ -38,28 +38,3 @@ class BankAccount:
     def check_balance(self):
         return self.balance
 
-# Working through a few examples of class working
-# Create a BankAccount object with the name "John's account" and an initial balance of 100
-account_john = BankAccount("John's account", 100)
-
-# Deposit 50 into the account
-account_john.deposit(50)
-print(account_john.check_balance())  # Outputs: 150
-
-account_mike = BankAccount("Mike's Account", 200)
-# Deposit 50 into the account
-account_mike.deposit(50)
-print(account_mike.check_balance())  # Outputs: 250
-
-# Withdraw 20 from the account
-account_john.withdraw(20)
-print(account_john.check_balance())  # Outputs: 130
-
-# Attempt to withdraw 250 from Jane's account
-# This will raise a ValueError because there are insufficient funds
-try:
-    account_john.withdraw(250)
-except ValueError as e:
-    print(e)  # Outputs: Insufficient funds.
-
-
--- main_test.py
+++ main_test.py
@@ -1,9 +1,6 @@
 import unittest
 from main import BankAccount
 
-
-
-
 class TestBankAccount(unittest.TestCase):
 
     def setUp(self):
@@ -11,8 +8,23 @@ class TestBankAccount(unittest.TestCase):
         # A fresh BankAccount object with a balance of 100
         self.account = BankAccount("Test Account", 100)
 
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_initialization(self):
+        # Test that a ValueError is raised when the initial balance is not a number
+        with self.assertRaises(ValueError):
+            BankAccount("Bad Account", "not a number")
+
+        # Test that a ValueError is raised when the initial balance is negative
+        with self.assertRaises(ValueError):
+            BankAccount("Bad Account", -100)
+
+    def test_deposit(self):
+        #need to fill this in
+
+    def test_withdraw(self):
+        #need to fill this in
+
+    def test_check_balance(self):
+        #need to fill this in
 
 
 if __name__ == '__main__':
